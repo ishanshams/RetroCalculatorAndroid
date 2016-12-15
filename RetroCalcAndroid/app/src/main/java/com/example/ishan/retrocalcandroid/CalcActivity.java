c9d8a9a8d1ba37415a07174c088a496b3e60dc6d
Ishan Shams <ishams@techtrend.us>
2016-12-15 14:01:19 -0500

    made updates on the color

--- RetroCalcAndroid/app/src/main/java/com/example/ishan/retrocalcandroid/CalcActivity.java
+++ RetroCalcAndroid/app/src/main/java/com/example/ishan/retrocalcandroid/CalcActivity.java
@@ -1,9 +1,10 @@
 package com.example.ishan.retrocalcandroid;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+
 import android.os.Bundle;
 
-public class CalcActivity extends AppCompatActivity {
+public class CalcActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
